3f01aa8e45af1a78910ab3705d10321d25fb503e
basharsenon <princemubashar276@gmail.com>
2025-05-22 20:05:33 +0500

    Update index.js with dot-only alias format

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+
 const app = express();
-const port = process.env.PORT || 3000;
-
-const aliasGenerator = require('./aliasGenerator');
-
 app.use(cors());
 
-app.get('/generate-alias', (req, res) => {
-    const alias = aliasGenerator.generateAlias();
-    res.json({ alias });
+app.get("/generate-alias", (req, res) => {
+  const base = "aliofficerecruitmentm185lhr";
+  const alias = base.replace(/([a-zA-Z])/g, "$1.") + "@gmail.com";
+  res.json({ alias });
 });
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
